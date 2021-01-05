636a2a1e08cce83baaf900a373976642ae8da896
CG-Tespy <gbcabreracgt@gmail.com>
2021-01-05 16:34:08 -0500

    Basic tuts roughly implemented, with save system functioning

---
+++ Assets/Scripts/SaveSystem/InventoryLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using CGTUnity.Fungus.SaveSystem;
+
+public class InventoryLoader : SaveLoader<InventoryData>
+{
+    public override bool Load(InventoryData saveData)
+    {
+        Debug.LogWarning("Load for InventoryLoader not implemented! Nothing happens.");
+        return true;
+    }
+}
---
+++ Assets/Scripts/SaveSystem/InventorySaver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using CGTUnity.Fungus.SaveSystem;
+
+public class InventorySaver : DataSaver<InventoryData>, ISaveCreator<InventoryData, ShopInventory>, IGroupSaver<InventoryData>
+{
+    [SerializeField] List<ShopInventory> toSave = null;
+
+    public override IList<SaveDataItem> CreateItems()
+    {
+        Debug.LogWarning("CreateItems for InventorySaver not implemented! Returning empty list.");
+        var temp = new List<SaveDataItem>();
+        return temp;
+    }
+
+    public InventoryData CreateSave(ShopInventory from)
+    {
+        Debug.LogWarning("CreateSave for InventorySaver not implemented! Returning base InventoryData.");
+        var temp = new InventoryData();
+        return temp;
+    }
+
+    public IList<InventoryData> CreateSaves()
+    {
+        Debug.LogWarning("CreateSaves (multiple!) for InventorySaver not implemented! Returning empty list.");
+        var temp = new List<InventoryData>();
+        return temp;
+    }
+
+}
