fc6602e35381a45189e527ae524d5d58ce43fc45
CG-Tespy <gbcabreracgt@gmail.com>
2021-01-13 10:18:47 -0500

    Fix to draggable2D not playing nice with mid-drag re-parenting

---
+++ Assets/Scripts/_Experimental/PlayWithCoordSystems.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWithCoordSystems : MonoBehaviour
+{
+    public Vector3 mouseCoords;
+    public Vector3 worldCoords;
+
+    private void Update()
+    {
+        worldCoords = Camera.main.ScreenToWorldPoint(mouseCoords);
+        
+    }
+}
