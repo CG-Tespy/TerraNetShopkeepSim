eb57533cca0264068aea3f998045b83941bdfeb9
CG-Tespy <gbcabreracgt@gmail.com>
2021-03-22 21:06:59 -0400

    Crafted battle cards immediately get added to card storage

--- Assets/Scripts/Commands/CraftItem.cs
+++ Assets/Scripts/Commands/CraftItem.cs
@@ -5,8 +5,18 @@ using Fungus;
 public class CraftItem : Command
 {
     [SerializeField] protected ItemData recipe;
+
     [VariableProperty(typeof(ItemVariable))]
     [SerializeField] protected ItemVariable itemVar = null;
+
+    [Tooltip("If what's crafted is a Battle Power, it will be assigned to this var. Otherwise, the var will be set to null.")]
+    [VariableProperty(typeof(ObjectVariable))]
+    [SerializeField] protected ObjectVariable battlePowerVar;
+
+    [Tooltip("Is set to true or false based on whether or not the crafted item is a battle power.")]
+    [VariableProperty(typeof(BooleanVariable))]
+    [SerializeField] protected BooleanVariable isBattlePower;
+
     [Tooltip("If this is empty, it will be set to one in the scene (if it exists).")]
     [SerializeField] protected CraftingSystem craftingSystem = null;
 
@@ -14,11 +24,35 @@ public class CraftItem : Command
     {
         base.OnEnter();
 
-        if (craftingSystem == null)
-            craftingSystem = FindObjectOfType<CraftingSystem>();
-
-        itemVar.Value = craftingSystem.Craft(recipe.Value as Recipe);
+        FetchCraftingSystemAsNeeded();
+        DoTheCrafting();
+        AssignToVarsAsNeeded();
 
         Continue();
+    }
+
+    protected virtual void FetchCraftingSystemAsNeeded()
+    {
+        if (craftingSystem == null)
+            craftingSystem = FindObjectOfType<CraftingSystem>();
+    }
+
+    protected virtual void DoTheCrafting()
+    {
+        craftResult = craftingSystem.Craft(recipe.Value as Recipe);
+    }
+
+    protected Item craftResult = null;
+
+    protected virtual void AssignToVarsAsNeeded()
+    {
+        if (itemVar != null)
+            itemVar.Value = craftResult;
+
+        if (battlePowerVar != null)
+            battlePowerVar.Value = craftResult as BattlePower;
+
+        if (isBattlePower != null)
+            isBattlePower.Value = battlePowerVar.Value != null;
     }
 }
